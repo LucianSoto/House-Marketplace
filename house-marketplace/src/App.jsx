21f1b1de2f5d340b763271dd7d8a29d7917a7ea3
luxian <luxianaiolos@gmail.com>
2022-06-08 11:25:39 -0700

    Implemented React Router

--- house-marketplace/src/App.jsx
+++ house-marketplace/src/App.jsx
@@ -1,10 +1,25 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Explore from './pages/Explore'
+import ForgotPassword from './pages/ForgotPassword'
+import Offers from './pages/Offers'
+import Profile from './pages/Profile'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
 
 function App() {
   return (
-    <div className="App">
-      <h1>House App</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/Offers' element={<Offers />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/sign-in' element={<Signin />} />
+          <Route path='/sign-up' element={<Signup />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ house-marketplace/src/pages/Offers.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Offers() {
+  return (
+    <div>
+      <h1>Offers</h1>
+    </div>
+  )
+}
+
+export default Offers
