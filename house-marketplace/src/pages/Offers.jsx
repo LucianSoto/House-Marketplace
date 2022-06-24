60529746df4ede85c38bfa6d0bb7f6df0757992d
LuxianAiolos <luxianaiolos@gmail.com>
2022-06-24 11:03:30 -0700

    Created ListingItem Component/Offers functionality

--- house-marketplace/src/pages/Offers.jsx
+++ house-marketplace/src/pages/Offers.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
+import {useParams} from 'react-router-dom'
+import {
+  collection, 
+  getDocs, 
+  query, 
+  where, 
+  orderBy, 
+  limit, 
+  startAfter
+} from 'firebase/firestore'
+import {db} from '../firebase.config'
+import{toast} from 'react-toastify'
+import Spinner from '../components/Spinner'
+import ListingItem from '../components/ListingItem'
 
-function Offers() {
+const Offers = () => {
+  const [listings, setListings] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const params = useParams()
+
+  useEffect(()=> {
+    const fetchListings = async () => {
+      try{
+        //Get Reference
+        const listingsRef = collection(db, 'listings')
+
+        //Create a query
+        const q = query(
+          listingsRef, 
+          where('offer', '==', true),
+          orderBy('timestamp', 'desc'),
+          limit(10)
+        )
+
+        // Execute query
+        const querySnap = await getDocs(q)
+
+        let listings = []
+      querySnap.forEach((doc)=> {
+        // console.log(doc.data())
+        return listings.push({
+          id: doc.id,
+          data: doc.data()
+        })
+      })
+
+      setListings(listings)
+      setLoading(false)
+      } catch (error){
+        console.log(error)
+        toast.error("Could not fetch listings")
+      }
+    }
+
+    fetchListings()
+  }, [])
+
   return (
-    <div>
-      <h1>Offers</h1>
+    <div className='category'>
+      <header>
+        <p className="pageheader">
+          Offers
+        </p>
+      </header> 
+
+      { loading ? 
+        ( <Spinner/> ) : listings && listings.length > 0 ?
+         ( <>
+          <main>
+            <ul className="categoryListings">
+              {listings.map((listing) => (
+                <ListingItem listing={listing.data} id={listing.id} key={listing.id}/>
+              ))}
+            </ul>
+          </main>
+         </> ) 
+         : ( <p>There are no current offers</p>)}
     </div>
   )
 }
