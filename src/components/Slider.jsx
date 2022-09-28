c1b6074703e7257511b655035cdcfd3f4309ebe1
LuxianAiolos <luxianaiolos@gmail.com>
2022-09-28 12:35:21 -0700

    Slider Component Finalized

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -15,10 +15,11 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import { Navigate } from 'react-router-dom';
 
 
 const Slider = () => {
-  const [images, setImages] = useState(null)
+  const [listings, setListings] = useState(null)
 
   useEffect(()=> {
     const  fetchImgUrls = async () => {
@@ -28,45 +29,56 @@ const Slider = () => {
         //Create a Query
         const q = query(
           listingsRef,
-          
           orderBy('timestamp', 'desc'),
           limit(10)
         )
 
         const querySnap = await getDocs(q)
 
-        let images = []
+        let listingsArr = []
 
         querySnap.forEach((doc) => {
-          // console.log(doc.data().imgUrls[0])
-          images.push(doc.data().imgUrls[0])
+          console.log(doc.data().imgUrls[0])
+          listingsArr.push({
+            type: doc.data().type,
+            img: doc.data().imgUrls,
+            price: doc.data().regularPrice,
+            name: doc.data().name,
+          })
         }) 
-        setImages(images)
+        setListings(listingsArr)
       } catch(err) {
         console.log('could not get images')
       }
     }
 
-
     fetchImgUrls()
   }, [])
 
-  console.log(images)
+  console.log(listings)
   return (
     <>
+    <h2>Recommended</h2>
     <Swiper
-      style={{height: "23vh"}}
+      style={{height: "25vh"}}
       slidesPerView={1}
       pagination={{clickable: true}}
     >
-      
-      {images && images.map((url, i) => (
-        <SwiperSlide key={i}>
+      {listings && listings.map((l, i) => (
+        <SwiperSlide 
+          key={i}
+          onClick={() => Navigate(`/category/${l.type}/${i}`)}
+        >
           <div 
             className="swiperSlideDiv"
-            style={{background: `url(${url}) center no-repeat`,
+            style={{background: `url(${l.img[0]}) center no-repeat`,
             backgroundSize: "cover",}}
           >
+            <p className="swiperSlideText">{l.name}</p>
+            <p className="swiperSlidePrice">
+              ${l.price}
+              {l.type === 'rent' &&  ' / Month'}
+            </p>
           </div>
         </SwiperSlide>
       ))}
